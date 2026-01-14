bd6e2a1e12220a986e82e02dd5998a88f587d5e7
Pongphisut Buahom <zcirpongphisut2549@gmail.com>
2026-01-14 18:39:55 +0700

    Add initial migration for RegisteredUser model with fields for username, email, password, and created_at

--- vercel/admin.py
+++ vercel/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import RegisteredUser
 
-# Register your models here.
+@admin.register(RegisteredUser)
+class RegisteredUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'created_at')
+    search_fields = ('username', 'email')
